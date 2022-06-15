4e29c1eb49ef8aa87e8f063561ce5330eb9bd47f
rohanntom <rohanntom14@gmail.com>
2022-06-15 14:45:50 +0530

    working

--- store-management/main.js
+++ store-management/main.js
@@ -5,137 +5,116 @@ class LineItem{
         this.productName = productName;
         this.quantity = quantity;
         this.mrp = mrp;
-    }
-    get amount(){
-        return (this.quantity * this.mrp).toFixed(2);
-    }
-    get tax(){
-        return (this.amount * 0.18).toFixed(2);
-    }
-    get amountWithTax(){
-        return (this.amount * 1.18).toFixed(2);
-    }
+        this.amount = (quantity * mrp).toFixed(2);
+        this.tax = (this.amount * 0.18).toFixed(2);
+        this.amountWithTax = (this.amount * 1.18).toFixed(2);
+     }
 }
 
 class Invoice{
-     static getLineItems(){
-        let lineItems;
-        // console.log(lineItems);
-
-        // if(localStorage.getItem('lineItems') === null){
-        //     lineItems = [];
-        // }else{
-        //     lineItems = JSON.parse(localStorage.getItem('lineItems'));
-        // }
-
-        if(lineItems === undefined){
-            lineItems = [];
-        }
- 
-        return lineItems;
-    } 
+    static lineItems = [];
+    static amount = 0;
+    static amountWithTax = 0;
+    static tax = 0;
 
     static addItem(productName,quantity,mrp){
         let lineItem = new LineItem(productName,quantity,mrp);
-        let lineItems = this.getLineItems();
-        lineItems.push(lineItem);
-        console.log(lineItems);
-        // console.log(Invoice.calcTotalAmount());
-        // localStorage.setItem('lineItems',JSON.stringify(lineItems));
+       
+        this.lineItems.push(lineItem);
+        this.amount += parseFloat(lineItem.amount);
+        this.amountWithTax += parseFloat(lineItem.amountWithTax); 
+        this.tax += parseFloat(lineItem.tax);
+       
     }
     static removeItem(productName){
-        let lineItems = Invoice.getLineItems();
-        lineItems.forEach((lineItem,index) => {
+        // console.log(productName);
+        // const pathOfItem = Object.keys(this.lineItems);
+        // const deleteItem = this.lineItems.indexOf(productName);
+        // if(deleteItem > -1){
+        //     this.lineItems.splice(deleteItem, 1);
+        //     // this.amount -= parseFloat(lineItem.amount);
+        //     // this.amountWithTax -= parseFloat(lineItem.amountWithTax); 
+        //     // this.tax -= parseFloat(lineItem.tax);
+        // }
+        // console.log(this.lineItems);
+       
+        console.log(productName);
+        this.lineItems.forEach((lineItem) => {
             if(lineItem.productName === productName){
-                lineItems.splice(index,1);
+                // this.lineItems.splice(lineItem, 1);
+                this.lineItems.pop(lineItem);
+                this.amount -= parseFloat(lineItem.amount);
+                this.amountWithTax -= parseFloat(lineItem.amountWithTax); 
+                this.tax -= parseFloat(lineItem.tax);
             }
+            console.log(this.lineItems);
         });
-        // console.log(lineItems);
-        //localStorage.setItem('lineItems', JSON.stringify(lineItems));
     }
-
-   static calcTotalAmount(){
-        let totalAmount = 0;
-        let lineItems = Invoice.getLineItems();
-        // console.log(lineItems);
-        // lineItems.forEach((lineItem) => totalAmount = totalAmount + lineItem.amount);
-        for(let lineItem in lineItems){
-            totalAmount = totalAmount + lineItem.amount;
-        }
-        return totalAmount;
-   }
-
-   static calcTotalAmountWithTax(){
-        let totalAmountWithTax = 0;
-        let lineItems = Invoice.getLineItems();
-        // console.log(lineItems);
-        for(let lineItem in lineItems){
-            totalAmountWithTax = totalAmountWithTax + lineItem.amountWithTax;
-        }
-        // console.log(totalAmountWithTax);
-        return totalAmountWithTax;
-   }
 }
 
 class StoreManagement{ 
-    static getInvoices(){
-        var invoices;
-        // if(localStorage.getItem('invoices') === null){
-        //     invoices=[];
-        // } else{
-        //     invoices = JSON.parse(localStorage.getItem('invoices'));
-        // }
-        if(invoices === undefined){
-            invoices=[];
-        }
-        return invoices;
-    }
-
-    // get invoiceId(){
-    //     return Date.now();
-    // }
+    static invoices = [];
+    static invoiceId = '';
+    static totalAmount = 0;
+    static totalAmountWithTax = 0;
+    static totalTax = 0;
+    static count = 0;
 
     static createInvoice(){
-        let invoice = Invoice.getLineItems();
-        let invoices = StoreManagement.getInvoices()
-        invoices.push(invoice);
-        // console.log(invoices);
-      //  localStorage.setItem('invoices',JSON.stringify(invoices));
+        let invoice = Invoice.lineItems;
+        this.invoices.push(invoice);
+        this.invoiceId = Date.now();
+        this.totalAmountWithTax += parseFloat(Invoice.amountWithTax);
+        this.totalAmount += parseFloat(Invoice.amount);
+        this.totalTax += parseFloat(Invoice.tax);
+        this.count++;
+        console.log(this.invoices);
     }
+
+    // static calcSales(){
+    //     this.invoices.forEach((invoice) =>{
+    //      invoice.
+
+    //     } );
+
+    // }
 
 }
 
 //UI
 class UI{
     static displayLineItems(){
-        let lineItems = Invoice.getLineItems();
-        // console.log(lineItems);
-        // if(this.lineItems === undefined){
+        let lineItems = Invoice.lineItems;  
+        // if(lineItems.length === 0){
         //     document.querySelector(".lineItem-listing").style.display = "none";
         //     document.querySelector("#emptyLineItem").style.display = "flex";
+        //     document.querySelector("#invoiceDiv").style.display="flex";
+        //     document.querySelector("#totalSales").style.display = "none";
+        //     document.querySelector("#allInvoices").style.display = "none";
+        //     document.querySelector("#inputForm").style.display = "none";
+        //     document.querySelector("#popupDiv").style.display = "none";
         // }
-        // else {
+        // else{
+        //     document.querySelector("#invoiceDiv").style.display="flex";
+        //     document.querySelector("#totalSales").style.display = "none";
+        //     document.querySelector("#allInvoices").style.display = "none";
+        //     document.querySelector("#inputForm").style.display = "none";
+        //     document.querySelector("#popupDiv").style.display = "none";
         //     document.querySelector(".lineItem-listing").style.display = "flex";
         //     document.querySelector("#emptyLineItem").style.display = "none";
         // }
-
-        
         
             for(let lineItem in lineItems){
                 return UI.addItemToList(lineItem);
             }
-     //   lineItems.forEach((lineItem) => UI.addItemToList(lineItem)); 
     }
 
 
     static addItemToList(lineItem){
         const list = document.querySelector('#lineItems-list');
-
         const row= document.createElement('tr');
 
-        
-
-        // <td>${UI.serialNumber+1}</td>
+        // <td>${Invoice.count}</td>
         row.innerHTML =`
         <td>${lineItem.productName}</td>
         <td>${lineItem.quantity}</td>
@@ -145,7 +124,6 @@ class UI{
         <td>${lineItem.amountWithTax}</td>
         <td><i class="fas fa-trash"></i></td>
         `;
-       // <td><i class="fas fa-pen"></i></td>
 
         list.appendChild(row);
     }
@@ -155,33 +133,33 @@ class UI{
             el.parentElement.parentElement.remove();
         }
     }
+    static clearLineItems(){
+        const list = document.querySelector('#lineItems-list');
+        const row= document.createElement('tr');
+        list.innerHTML ="";
+        row.innerHTML ="";
+        document.querySelector('#viewTotal').innerHTML=`Total Amount: ${0.00}`;
+    }
 
     static clearFields(){
         document.querySelector('#productName').value='';
         document.querySelector('#quantity').value='';
         document.querySelector('#mrp').value='';
     }
-    
+
     static displayInvoices(){
-     const invoices = StoreManagement.getInvoices();
-     invoices.forEach((invoice)=>UI.addInvoice(invoice));
+        let invoiceList =document.querySelector('#invoice-list');
+        let row= document.createElement('tr');
+
+        row.innerHTML =`
+        <td>${StoreManagement.count}</td>
+        <td>${StoreManagement.invoiceId}</td>
+        <td>${Invoice.amountWithTax.toFixed(2)}</td>
+        `;
+
+        invoiceList.appendChild(row);
     }
 
-    static addInvoice(invoice){
-    const invoiceList =document.querySelector('#invoice-list');
-
-    const row= document.createElement('tr');
-
-    row.innerHTML =`
-    <td>${Date.now()}</td>
-    <td>${invoice.calcTotalAmountWithTax}</td>
-    `;
-    // <td>${invoice.slNo}</td>
-    // <td><i class="fas fa-trash"></i></td>
-    // <td><i class="fas fa-pen"></i></td>
-
-    invoiceList.appendChild(row);
-}
     static showAlert(message, className) {
         const div = document.createElement('div');
         div.className =`alert alert-${className}`;
@@ -196,7 +174,7 @@ class UI{
 
 
 // Shows total amount inside the container
-document.querySelector('#viewTotal').innerHTML=`Total Amount: ${Invoice.calcTotalAmountWithTax()}`;
+document.querySelector('#viewTotal').innerHTML=`Total Amount: ${Invoice.amountWithTax.toFixed(2)}`;
 
 // Event: Create new invoice
 document.querySelector("#newInvoice").addEventListener("click",showInvoice);
@@ -228,23 +206,22 @@ function addItemFunction(){
 document.querySelector("#inputForm").addEventListener("submit",
 (e)=>{
     e.preventDefault();
-    const productName = document.querySelector('#productName').value;
-    const quantity = document.querySelector('#quantity').value;
-    const mrp = document.querySelector('#mrp').value;
+    let productName = document.querySelector('#productName').value;
+    let quantity = document.querySelector('#quantity').value;
+    let mrp = document.querySelector('#mrp').value;
 
     if(productName === '' || quantity === '' || mrp === ''){
         // UI.showAlert("Please fill all fields", "danger");
         alert("Please fill all fields");
     }
     else{
-        const lineItem = new LineItem(productName,quantity,mrp);
+        let lineItem = new LineItem(productName,quantity,mrp);
         UI.addItemToList(lineItem);
         Invoice.addItem(productName,quantity,mrp);
-        // StorageEvent.addLineItem(lineItem);
-
         //UI.showAlert("Item Added", 'success');
         alert('Item Added');
         UI.clearFields();
+        document.querySelector('#viewTotal').innerHTML=`Total Amount: ${Invoice.amountWithTax.toFixed(2)}`;
         this.showInvoice();
     }
 }
@@ -257,35 +234,42 @@ document.querySelector('#lineItems-list').addEventListener('click',(e)=>{
 
     // delete lineItem from array
     Invoice.removeItem(e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent);
-    
+    // Shows total amount inside the container
+    document.querySelector('#viewTotal').innerHTML=`Total Amount: ${Invoice.amountWithTax.toFixed(2)}`;
     // UI.showAlert('Item removed','danger');
     alert('Item Removed');
    }
 })
 
 // Event: Submit invoice
-document.querySelector("#invoiceCompleted").addEventListener("click",submitInvoice);
-function submitInvoice(){
+document.querySelector("#checkout").addEventListener("click",submitInvoice);
+
+    function submitInvoice(){
+        if(Invoice.lineItems.length === 0){
+            alert('Invoice is empty!!! Add an item.')
+        }
+        else{
     document.querySelector("#invoiceDiv").style.display="none";
     document.querySelector("#popupDiv").style.display = "flex";
     document.querySelector("#allInvoices").style.display = "none";
     document.querySelector("#totalSales").style.display = "none";
     document.querySelector("#inputForm").style.display = "none";
+    // console.log(StoreManagement.invoices);
 
-    // Show invoices in UI
-    UI.displayInvoices();  
     // Add invoice to array  
     StoreManagement.createInvoice();
-}
 
-
-
-// Shows popup container
-const displayPopup = document.querySelector('.popup-container');
-displayPopup.innerHTML = `<h2>Your total is $${Invoice.calcTotalAmountWithTax()}.</h2>
-<h2>Thanks for shopping.</h2>`;
-/* <input type="submit" value="Done" class="button">`; */
-
+    // Shows popup container
+    const displayPopup = document.querySelector('.popup-container');
+    displayPopup.innerHTML = `<h2>Your total is $${Invoice.amountWithTax.toFixed(2)}.</h2>
+    <h2>Thanks for shopping.</h2>`;
+    // <input type="submit" value="Done" class="button">`; 
+    UI.clearLineItems();
+    Invoice.lineItems = [];
+    // Show invoices in UI
+    UI.displayInvoices();  
+    }
+    }
 
 // Event: View total sales button
 document.querySelector("#viewSales").addEventListener("click",showTotalSales);
@@ -295,21 +279,15 @@ function showTotalSales(){
     document.querySelector("#totalSales").style.display = "flex";
     document.querySelector("#inputForm").style.display = "none";
     document.querySelector("#popupDiv").style.display = "none"; 
+
+    // View total sales
+    const displayTotalSales = document.querySelector('#totalSales');
+    displayTotalSales.innerHTML = `<h1>Total Sales</h1>
+    <h2>Total number of invoices: ${StoreManagement.invoices.length}</h2> 
+    <h2>Total Sales: ${StoreManagement.totalAmountWithTax.toFixed(2)}</h2>
+    <h2>Total Tax: ${StoreManagement.totalTax.toFixed(2)}</h2>
+    <h2>Total Revenue: ${StoreManagement.totalAmount.toFixed(2)}</h2>`;
 }
-
-
-
-// View total sales
-const displayTotalSales = document.querySelector('#totalSales');
-
-displayTotalSales.innerHTML = `<h1>Total Sales</h1>
-<h2>Total Sales: ${Invoice.calcTotalAmountWithTax()}</h2>
-<h2>Total Tax: ${Invoice.calcTotalAmount()}</h2>
-<h2>Total Revenue: ${Invoice.calcTotalAmountWithTax() - Invoice.calcTotalAmount()}</h2>`;
-// <h2>Total number of invoices: ${Invoice.getLineItems().length}</h2> 
-// Event: Display invoices
-// var uniqueId = Date.now();
-// document.addEventListener('DOMContentLoaded',UI.displayInvoices);
 
 // Event: View all invoices button
 document.querySelector("#viewInvoices").addEventListener("click",showAllInvoices);
@@ -319,7 +297,8 @@ function showAllInvoices(){
     document.querySelector("#allInvoices").style.display = "flex";
     document.querySelector("#inputForm").style.display = "none";
     document.querySelector("#popupDiv").style.display = "none";
+
+    // // Show all invoices
+    // document.addEventListener('DOMContentLoaded',UI.displayInvoices);
 }
 
-// Show all invoices
-document.addEventListener('DOMContentLoaded',UI.displayInvoices);
